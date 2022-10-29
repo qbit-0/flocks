986467481f3bd156b18710a36e862a727fd34796
Duy Pham <duypham12241999@gmail.com>
2022-10-28 17:21:40 -0700

    Update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,24 @@ export default function Home() {
     <Box component="div">
       <Head>
         <title>Flocks</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Box component="div" display="flex" width="100%" height="100vh">
         <FlockDemo />
