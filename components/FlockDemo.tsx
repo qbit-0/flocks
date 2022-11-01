224f0264f5b3ee02d91dc7c111b612accad210e7
Duy Pham <duypham12241999@gmail.com>
2022-10-31 17:06:38 -0700

    check for web gl

--- components/FlockDemo.tsx
+++ components/FlockDemo.tsx
@@ -1,14 +1,13 @@
 import { Typography } from "@mui/material";
 import FlocksContextProvider from "../utils/context/FlocksContextProvider";
-import useDetectWebGl from "../utils/hooks/useDetectWebGl";
+import WebGL from "../utils/WebGL";
 import FlocksCanvas from "./FlocksCanvas";
 import FlocksControls from "./FlocksControls";
 
 type Props = {};
 
 const FlockDemo = (props: Props) => {
-  const isDetectWebGl = useDetectWebGl();
-  if (!isDetectWebGl) {
+  if (!WebGL.isWebGLAvailable()) {
     return <Typography>WebGl is not supported on this browser. </Typography>;
   }
 
